905e02dde934ff02f07c6445614aba98b28c707d
phanh <anhtp@nftech.io>
2025-10-11 09:26:40 +0700

    add database config

--- src/main/java/com/example/meal_tracker/MealTrackerApplication.java
+++ src/main/java/com/example/meal_tracker/MealTrackerApplication.java
@@ -2,10 +2,8 @@ package com.example.meal_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class MealTrackerApplication {
 
     public static void main(String[] args) {
